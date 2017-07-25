c5ab854065556f7232598d2918b8d07bbcd6972a
z i λ a s a l <sarikayaziya@gmail.com>
2017-07-25 12:07:40 +0200

    Update ITodoService.cs

--- MSTodoApi/Infrastructure/ITodoService.cs
+++ MSTodoApi/Infrastructure/ITodoService.cs
@@ -6,6 +6,9 @@ namespace MSTodoApi.Infrastructure
 {
     public interface ITodoService
     {
-        Task<OperationResult<TodosViewModel>> GetTodos(DateTime dueDateTime, bool includeOverdueTasks = false, string taskFields = "", string eventFields = "", bool includeCancelledEvents = false);
+        Task<OperationResult<TodosViewModel>> GetTodos(DateTime dueDateTime, 
+                                                       bool includeOverdueTasks = false, 
+                                                       string taskFields = "", string eventFields = "", 
+                                                       bool includeCancelledEvents = false);
     }
 }
